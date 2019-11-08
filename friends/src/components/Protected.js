0f3d382f5be76e48ea0988a79715afc2f56582ce
triggering-event <gabriel.j.shapiro@gmail.com>
2019-11-07 23:52:46 -0500

    adumbrates basic components and routes for protectedroute pattern

---
+++ friends/src/components/Protected.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Protected = () => {
+    return (
+        <div>
+            <p>This is the Protected page</p>
+        </div>
+    );
+};
+
+export default Protected;
---
+++ friends/src/components/Public.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Public = () => {
+    return (
+        <div>
+            <p>This is the Public page</p>
+        </div>
+    );
+};
+
+export default Public;

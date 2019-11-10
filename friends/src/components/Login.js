c3e86a266b0ad893e61c07e2d218b31c23a8a034
triggering-event <gabriel.j.shapiro@gmail.com>
2019-11-10 00:53:15 -0500

    adds login page

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {axiosWithAuth} from "../axiosWithAuth";
 
-const Login = () => {
+const Login = (props) => {
+    const [credentials, setCredentials] = useState({});
+
+    const login = e => {
+        e.preventDefault();
+        axiosWithAuth().post('login/endpoint', credentials)
+            .then(res => {
+                localStorage.setItem('token', res.data.token);
+                this.props.history.push('/');
+            })
+    }
+
+    const handleChange = e => {
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value
+        })
+    }
+
     return (
         <div>
             <p>This is the Login page</p>
+            <form onSubmit={login}>
+                <input type="text" name="username" value={credentials.username} onChange={handleChange}/>
+                <input type="password" name="password" value={credentials.password} onChange={handleChange}/>
+                <button>Log in</button>
+            </form>
         </div>
     );
 };
